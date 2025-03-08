0e65e3d3492ae534f8f9c13bb1324078e6d57167
alej0nt <alej0nt666@gmail.com>
2025-03-08 01:46:18 -0500

    Terminada clase abstracta

--- src/main/java/Main/SistemaDeTransportee.java
+++ src/main/java/Main/SistemaDeTransportee.java
@@ -1,13 +1,5 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
-
 package Main;
 
-/**
- *
- * @author PC
- */
 public class SistemaDeTransportee {
 
     public static void main(String[] args) {
